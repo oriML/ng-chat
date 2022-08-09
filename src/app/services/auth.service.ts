a98e62681ae7fda7d9c427366effa093a02049b3
Ori Menachem Levi <orimenachemlevi@gmail.com>
2022-08-09 23:44:50 +0300

    add auth.service + init config

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  
+  constructor(
+    private afs: AngularFirestore,
+    private afAuth: AngularFireAuth
+  ) { }
+
+}
